311baf140f4d21ae8e571f78bb1e7a383bd04cd1
李炅倫 <openbox008@gmail.com>
2021-08-01 11:31:02 +0800

    First commit code example

---
+++ scripts/dbInitialize.py
@@ -0,0 +1,32 @@
+import hashlib
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root123@192.168.1.107:3306/DTM'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(45), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean, default=True, nullable=False)
+
+    def __init__(self, username=None, password=None, active=True):
+        self.username = username
+        self.password = password
+        self.active = True
+
+
+
+db.create_all()
+
+hash_password = hashlib.sha256(b"123456").hexdigest()
+user = User(username="admin@gmail.com", password=hash_password)
+db.session.add(user)
+db.session.commit()
+
+
+db.session.close()
