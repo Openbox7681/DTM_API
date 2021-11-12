872f55ca0a2dc235ec5eff4b6da1601b10348eed
李炅倫 <openbox008@gmail.com>
2021-11-12 17:00:35 +0800

    update

--- app/model/SystemInfo.py
+++ app/model/SystemInfo.py
@@ -3,6 +3,7 @@ import socket
 import dns.resolver
 import time
 import os 
+import json
 
 #取得Inbound 資訊
 def get_bound_info():
@@ -254,7 +255,7 @@ def get_dtm_log(size):
     for log in loglines:
         res = dict()
         res = {
-            "data" : log
+            "data" : json.loads(log)
         }
         response.append(res)
     return response
