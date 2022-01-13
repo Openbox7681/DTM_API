89cac77c22dc7352b3898eccd98d943465163547
Jim <jim.liu@angelsctek.com>
2022-01-13 12:03:32 +0800

    add network fetching

--- app/controller/NetworkController.py
+++ app/controller/NetworkController.py
@@ -56,7 +56,7 @@ class ShutdownService(Resource):
         return jsonify({
             "Status": status,
             "Message": message,
-            "Data": data
+            "Data": data,
         })
 
 
@@ -104,56 +104,98 @@ class NetworkService(Resource):
         self.logger = kwargs.get('logger')
 
     def get(self):
+        networkMap = {}
+        # get dns list
+        dnsBlocks = os.popen('systemd-resolve --status').read().split('\n\n')
+        for dnsNetwork in dnsBlocks:
+            if not dnsNetwork.startswith('Link'):
+                continue
+            lines = dnsNetwork.split('\n')
+            name = lines[0].split('(')[1].split(')')[0]
+            networkMap[name] = {}
+            networkMap[name]['DNS'] = []
+            for dns in dnsNetwork.split('DNS Servers:')[1].split('\n'):
+                networkMap[name]['DNS'].append(dns.strip())
+
+        # get gateway
+        gatewayBlocks = os.popen('nmcli dev show').read().split('\n\n')
+        for gatewayLines in gatewayBlocks:
+            for line in gatewayLines.split('\n'):
+                if 'GENERAL.DEVICE' in line:
+                    name = line.split(' ')[-1]
+                if 'IP4.GATEWAY' in line and name in networkMap:
+                    gateway = line.split(' ')[-1]
+                    networkMap[name]['Gateway'] = gateway
+
+        # get network info
+        networkList = []
+        ifconfig = os.popen('ifconfig').read().strip().split('\n\n')
+        for networkString in ifconfig:
+            network = {}
+            lines = networkString.split('\n')
+            network['Name'] = lines[0].split(':')[0]
+            secondLines = lines[1].strip().split(' ')
+            network['Ip'] = secondLines[1]
+            network['Netmask'] = secondLines[4]
+            network['Mac'] = lines[3].strip().split(' ')[1]
+            network['DNS'] = networkMap[network['Name']]['DNS']
+            network['Gateway'] = networkMap[network['Name']]['Gateway']
+            networkList.append(network)
+
+
         status = 200
         message = 'success'
-        data = [
-            {
-                'Name': 'enp0s8',
-                'Ip': '192.168.0.202',
-                'Netmask': '255.255.255.0',
-                'Mac': '08:00:27:66:ef:5a',
-                'Trafic': {
-                    'Max': 2000,
-                    'Min': 100,
-                    'Average': 1400,
-                    'List': [
-                        {'timestamp': 1641318528, 'flow': 200},
-                        {'timestamp': 1641318528, 'flow': 100},
-                        {'timestamp': 1641318528, 'flow': 600},
-                        {'timestamp': 1641318528, 'flow': 800},
-                        {'timestamp': 1641318528, 'flow': 2000},
-                        {'timestamp': 1641318528, 'flow': 1800},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                    ]
-                }
-            },
-            {
-                'Name': 'enp0s9',
-                'Ip': '192.168.0.203',
-                'Netmask': '255.255.255.0',
-                'Mac': '08:00:27:8b:76:f3',
-                'Trafic': {
-                    'Max': 2000,
-                    'Min': 100,
-                    'Average': 1400,
-                    'List': [
-                        {'timestamp': 1641318528, 'flow': 200},
-                        {'timestamp': 1641318528, 'flow': 100},
-                        {'timestamp': 1641318528, 'flow': 600},
-                        {'timestamp': 1641318528, 'flow': 800},
-                        {'timestamp': 1641318528, 'flow': 2000},
-                        {'timestamp': 1641318528, 'flow': 1800},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                        {'timestamp': 1641318528, 'flow': 1600},
-                    ]
-                }
-            }
-        ]
+        data = networkList
+        # data = [
+        #     {
+        #         'Name': 'enp0s8',
+        #         'Ip': '192.168.0.202',
+        #         'Netmask': '255.255.255.0',
+        #         'Mac': '08:00:27:66:ef:5a',
+        #         'DNS': [''],
+        #         'Gateway': '',
+        #         'Trafic': {
+        #             'Max': 2000,
+        #             'Min': 100,
+        #             'Average': 1400,
+        #             'List': [
+        #                 {'timestamp': 1641318528, 'flow': 200},
+        #                 {'timestamp': 1641318528, 'flow': 100},
+        #                 {'timestamp': 1641318528, 'flow': 600},
+        #                 {'timestamp': 1641318528, 'flow': 800},
+        #                 {'timestamp': 1641318528, 'flow': 2000},
+        #                 {'timestamp': 1641318528, 'flow': 1800},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #             ]
+        #         }
+        #     },
+        #     {
+        #         'Name': 'enp0s9',
+        #         'Ip': '192.168.0.203',
+        #         'Netmask': '255.255.255.0',
+        #         'Mac': '08:00:27:8b:76:f3',
+        #         'Trafic': {
+        #             'Max': 2000,
+        #             'Min': 100,
+        #             'Average': 1400,
+        #             'List': [
+        #                 {'timestamp': 1641318528, 'flow': 200},
+        #                 {'timestamp': 1641318528, 'flow': 100},
+        #                 {'timestamp': 1641318528, 'flow': 600},
+        #                 {'timestamp': 1641318528, 'flow': 800},
+        #                 {'timestamp': 1641318528, 'flow': 2000},
+        #                 {'timestamp': 1641318528, 'flow': 1800},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #                 {'timestamp': 1641318528, 'flow': 1600},
+        #             ]
+        #         }
+        #     }
+        # ]
         return jsonify({
             "Status": status,
             "Message": message,
