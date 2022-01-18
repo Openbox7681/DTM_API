927d5cd416554da51307d0b98e03b346439729f5
Jim <jim.liu@angelsctek.com>
2022-01-19 00:38:04 +0800

    add detection post

--- app/controller/DashBoardController.py
+++ app/controller/DashBoardController.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import create_access_token, jwt_required ,get_jwt_identi
 import app.model.SystemInfo as system
 from datetime import datetime
 from app import jwt
+import os
 
 class GetCpuInfo(Resource):
     def __init__(self, **kwargs):
@@ -48,12 +49,47 @@ class GetDetection(Resource):
             message = 'error'
             data = str(e)
 
+        return jsonify({
+            "Status": status,
+            "Message": message,
+            "Data": data
+        })
+
+    def post(self):
+        args = reqparse.RequestParser() \
+            .add_argument('Detection', type=str, location='json', required=True) \
+            .parse_args()
+        message = None
+        status = None
+        data = dict()
+        detection = args['Detection']
+        try:
+            lines = os.popen('cat /etc/suricata/suricata.yaml').read().split('\n')
+            newLines = []
+            doReplace = False
+            for line in newLines:
+                if doReplace:
+                    newLines.append(f'    HOME_NET: {detection}')
+                    doReplace = False
+                    continue
+                if 'address-groups' in line:
+                    doReplace = True
+            with open('/etc/dtm/build/dtm.config', 'w') as f:
+                f.write('\n'.join(newLines))
+            status = 200
+            message = 'success'
+            data = system.get_detection()
+        except Exception as e:
+            status = 201
+            message = 'error'
+            data = str(e)
 
         return jsonify({
             "Status": status,
             "Message": message,
             "Data": data
         })
+
 
 class GetMemoryInfo(Resource):
     def __init__(self, **kwargs):
