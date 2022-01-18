84384096def723c9dfe5ae8c8b3603c1d0d9d85e
Jim <jim.liu@angelsctek.com>
2022-01-19 00:41:52 +0800

    fix wrong detection path

--- app/controller/DashBoardController.py
+++ app/controller/DashBoardController.py
@@ -74,7 +74,7 @@ class GetDetection(Resource):
                     continue
                 if 'address-groups' in line:
                     doReplace = True
-            with open('/etc/dtm/build/dtm.config', 'w') as f:
+            with open('/etc/suricata/suricata.yaml', 'w') as f:
                 f.write('\n'.join(newLines))
             status = 200
             message = 'success'
